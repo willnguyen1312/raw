e54f81a50b0fc997d86b54e4995b89a5d0e3de0b
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-22 16:49:29 +0700

    Aha

---
+++ app.js
@@ -0,0 +1,3 @@
+import map from "https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/map.js";
+
+console.log(map);
---
+++ index.js
@@ -0,0 +1,2 @@
+import lodashEs from "http://cdn.skypack.dev/lodash-es";
+console.log(lodashEs.range(4));
